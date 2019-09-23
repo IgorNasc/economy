e9a17888b93087984e140e7d4ddb2558748126b1
AndradI1 <[Igor.Andrade@unisys.com]>
2019-09-23 11:25:26 -0300

    Adicionado o package do calendario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/* CALENDAR */
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+
+/* ROUTING */
+import { AppRoutingModule } from './app-routing.module';
+
+/* CHARTS */
+import { ChartsModule } from 'ng2-charts';
 
 import { AppComponent } from './app.component';
-import { ChartsModule } from 'ng2-charts';
 import { HomeComponent } from './home/home.component';
-import { AppRoutingModule } from './app-routing.module';
 import { ExtratoComponent } from './extrato/extrato.component';
 
 @NgModule({
@@ -16,7 +25,12 @@ import { ExtratoComponent } from './extrato/extrato.component';
   imports: [
     BrowserModule,
     ChartsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
