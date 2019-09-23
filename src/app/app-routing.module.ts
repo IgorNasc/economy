b85012b16fe5f3622ff4002a87a7fc89fbfb5f6b
AndradI1 <[Igor.Andrade@unisys.com]>
2019-09-23 15:17:20 -0300

    Criado a pagina com o calendario funcional

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ExtratoComponent } from './extrato/extrato.component';
+import { AgendamentoComponent } from './agendamento/agendamento.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'extrato', component: ExtratoComponent }
+  { path: 'extrato', component: ExtratoComponent },
+  { path: 'agendamento', component: AgendamentoComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 /* CALENDAR */
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { FlatpickrModule } from 'angularx-flatpickr';
 
 /* ROUTING */
 import { AppRoutingModule } from './app-routing.module';
@@ -15,18 +17,26 @@ import { ChartsModule } from 'ng2-charts';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ExtratoComponent } from './extrato/extrato.component';
+import { AgendamentoComponent } from './agendamento/agendamento.component';
+import { CommonModule } from '@angular/common';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ExtratoComponent
+    ExtratoComponent,
+    AgendamentoComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
+    FormsModule,
     ChartsModule,
     AppRoutingModule,
+    NgbModalModule,
     BrowserAnimationsModule,
+    FlatpickrModule.forRoot(),
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory
